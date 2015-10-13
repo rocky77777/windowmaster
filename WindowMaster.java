1f1bfd89664c62ea19a7f241a7ab5be5b949cb1b
Bill H <pacmanthepirate@gmail.com>
2015-10-12 22:22:52 -0600

    added function to input price

--- WindowMaster.java
+++ WindowMaster.java
@@ -25,20 +25,24 @@ public class WindowMaster {
         System.out.println("What's the width of the window?");
         int wWidth = Integer.parseInt(sc.nextLine());
         
+        System.out.println("How much does window glass cost per sq. ft.?");
+        double windowCostPerSqFt = Double.parseDouble(sc.nextLine());
+        System.out.println("How much does trim cost per ft.?");
+        double trimCostPerFt = Double.parseDouble(sc.nextLine());
+        
         //dimension calculations
         int area = wHeight * wWidth;
         int perimeter = (wHeight * 2) + (wWidth * 2);
         
         //cost calculations
-        double windowCost = area * 3.50;
-        double trimCost = perimeter * 2.25;
+        double totalWindowCost = area * windowCostPerSqFt;
+        double totalTrimCost = perimeter * trimCostPerFt;
         
-        double total = windowCost + trimCost;
+        double total = totalWindowCost + totalTrimCost;
         
-        System.out.println("Your windows cost: " + windowCost);
-        System.out.println("and your trim costs: " + trimCost);
+        System.out.println("Your windows cost: " + totalWindowCost);
+        System.out.println("and your trim costs: " + totalTrimCost);
         System.out.println("Your total cost is: " + total);
-        System.out.println("Thank you.");
         System.out.println("Thank you.");
         
     }
